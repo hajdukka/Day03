15de04a95cfeeb28c6c59d3637d4fa316fdd0565
hajdukka <nerminhajdo@gmail.com>
2021-03-10 20:46:36 +0100

    dodao file sa komentarima

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    // Only change code below this line
+
+    // Komentar u jednoj liniji
+
+    /* komentar
+    u
+    dvije ili
+    vise 
+    linija*/
+
+    // Only change code above this line
+};
+
+console.log(main());
+
+module.exports = main;
